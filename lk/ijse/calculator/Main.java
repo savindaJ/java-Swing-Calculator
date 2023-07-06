94f91495ee098688a9524140701073bd343983a7
SavindaJayasekara <thantrige32@gmail.com>
2023-07-06 10:26:55 +0530

    add panels

--- lk/ijse/calculator/Main.java
+++ lk/ijse/calculator/Main.java
@@ -1,6 +1,15 @@
 package lk.ijse.calculator;
 
+import javax.swing.*;
+
 public class Main {
+    private JPanel buttonPanel;
+    private JPanel displayPanel;
+    private JPanel namePanel;
+    private JPanel downPanel;
+    private JPanel lblPanel;
+
+
     public static void main(String[] args) {
 
     }
