2b94556ecfbd069bacb9063652f7e96fb948a5c9
SavindaJayasekara <thantrige32@gmail.com>
2023-07-06 10:29:40 +0530

    add operators and change object

--- lk/ijse/calculator/Main.java
+++ lk/ijse/calculator/Main.java
@@ -1,14 +1,6 @@
 package lk.ijse.calculator;
 
-import javax.swing.*;
-
 public class Main {
-    private JPanel buttonPanel;
-    private JPanel displayPanel;
-    private JPanel namePanel;
-    private JPanel downPanel;
-    private JPanel lblPanel;
-
 
     public static void main(String[] args) {
 
--- lk/ijse/calculator/obj/Calculator.java
+++ lk/ijse/calculator/obj/Calculator.java
@@ -6,6 +6,19 @@ import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame implements ActionListener {
 
+    private JPanel buttonPanel;
+    private JPanel displayPanel;
+    private JPanel namePanel;
+    private JPanel downPanel;
+    private JPanel lblPanel;
+
+
+    private String operator;
+    private String numberOne;
+    private String numberTwo;
+    private double num1;
+    private double num2;
+    private double result;
     @Override
     public void actionPerformed(ActionEvent e) {
 
